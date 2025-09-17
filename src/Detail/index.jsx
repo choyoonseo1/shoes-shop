65a1fe596bc7211c21207ad7021fb870895b0ee1
choyoonseo12 <choyoonseo1@icloud.com>
2025-09-17 11:20:18 +0900

    커밋

---
+++ src/Detail/index.jsx
@@ -0,0 +1,42 @@
+import { useNavigate, useParams } from "react-router-dom";
+
+function Detail(product) {
+  // detail/3 -> pathVariable 값을 확인...
+  // hook : useParams
+  // 파라미터를 변수로 저장할 때는 중괄호 사용 필수
+  let {id} = useParams(); // 얘는 문자값
+  const navigate = useNavigate();
+  // 가져온 pathVariable값을 숫자로 바꿔줘야함
+  // props로 전달받은 product 배열에서 해당하는 객체만 찾음
+  const findProduct = product.find(item => {
+    return item.id === Number(id)
+  })
+
+  // 해당하는 제품이 존재하지 않을 때 처리
+  if(findProduct == null){
+    alert('찾는 상품이 없습니다.')
+    // 바로 이전 페이지로 이동
+    // history.back(); - 자바스크립트용
+    navigate(-1);
+    return null;
+  } 
+
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img src={`/images/shoes${findProduct.id+1}.jpg`}
+            width="100%" />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{findProduct.id}</h4>
+          <p>{findProduct.content}</p>
+          <p>{findProduct.price}</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Detail;
